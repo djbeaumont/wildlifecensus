e8812ff884319b4acbe4ca146f7518fd92d10370
David Beaumont <david@zorq.net>
2011-06-25 15:11:24 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wildlifecensus::Application.config.secret_token = '469bb5df4a3323ffd9a4c3ec9924af96ccfff7ba545247818d0b966ab6b23822ed0f6366f1c70ddf9c54ab3bedf6843a9e175bf2927f79e59b63fc3657e7527a'
